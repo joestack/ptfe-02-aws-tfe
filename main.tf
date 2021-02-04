f4bff0a3c8a65e781e23f7f06147ca51ff1674f8
joestack <joern@joern-online.de>
2021-02-04 15:31:54 +0100

    9

--- main.tf
+++ main.tf
@@ -56,12 +56,12 @@ module "tfe" {
   tfe_hostname                  = "${var.hostname}.${var.dns_zone}"
   console_password              = "aws_secretsmanager"
   enc_password                  = "aws_secretsmanager"
-  aws_secretsmanager_secret_arn = data.terraform_remote_state.remote.aws_secret_arn
+  aws_secretsmanager_secret_arn = "data.terraform_remote_state.remote.aws_secret_arn"
   
-  vpc_id                     = data.terraform_remote_state.remote.vpc_id
-  alb_subnet_ids             = data.terraform_remote_state.remote.alb_subnet_ids # private subnet IDs
-  ec2_subnet_ids             = data.terraform_remote_state.remote.ec2_subnet_ids # private subnet IDs
-  rds_subnet_ids             = data.terraform_remote_state.remote.rds_subnet_ids # private subnets IDs
+  vpc_id                     = "data.terraform_remote_state.remote.vpc_id"
+  alb_subnet_ids             = "data.terraform_remote_state.remote.alb_subnet_ids" # private subnet IDs
+  ec2_subnet_ids             = "data.terraform_remote_state.remote.ec2_subnet_ids" # private subnet IDs
+  rds_subnet_ids             = "data.terraform_remote_state.remote.rds_subnet_ids" # private subnets IDs
   load_balancer_is_internal  = true
   route53_hosted_zone_public = var.dns_zone
 
