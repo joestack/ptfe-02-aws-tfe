c1ae1a2420a839c9514916a63524b8ea5e6ee1cb
joestack <joern@joern-online.de>
2021-02-04 15:46:44 +0100

    13

--- main.tf
+++ main.tf
@@ -45,28 +45,28 @@ data "terraform_remote_state" "remote" {
 
 
 
-module "tfe" {
-  source = "git@github.com:joestack/is-terraform-aws-tfe-standalone.git"
+# module "tfe" {
+#   source = "git@github.com:joestack/is-terraform-aws-tfe-standalone.git"
 
-  friendly_name_prefix       = var.name_prefix
+#   friendly_name_prefix       = var.name_prefix
 
   
-  tfe_bootstrap_bucket          = "data.terraform_remote_state.remote.tfe_bootstrap_bucket"
-  tfe_license_filepath          = "s3://data.terraform_remote_state.remote.tfe_bootstrap_bucket/tfe-license.rli"
-  tfe_hostname                  = "${var.hostname}.${var.dns_zone}"
-  console_password              = "aws_secretsmanager"
-  enc_password                  = "aws_secretsmanager"
-  aws_secretsmanager_secret_arn = "data.terraform_remote_state.remote.aws_secret_arn"
+#   tfe_bootstrap_bucket          = "data.terraform_remote_state.remote.tfe_bootstrap_bucket"
+#   tfe_license_filepath          = "s3://data.terraform_remote_state.remote.tfe_bootstrap_bucket/tfe-license.rli"
+#   tfe_hostname                  = "${var.hostname}.${var.dns_zone}"
+#   console_password              = "aws_secretsmanager"
+#   enc_password                  = "aws_secretsmanager"
+#   aws_secretsmanager_secret_arn = "data.terraform_remote_state.remote.aws_secret_arn"
   
-  vpc_id                     = "data.terraform_remote_state.remote.vpc_id"
-  alb_subnet_ids             = split(",", "data.terraform_remote_state.remote.alb_subnet_ids") # private subnet IDs
-  ec2_subnet_ids             = tolist(data.terraform_remote_state.remote.ec2_subnet_ids) # private subnet IDs
-  rds_subnet_ids             = "data.terraform_remote_state.remote.rds_subnet_ids" # private subnets IDs
-  load_balancer_is_internal  = true
-  route53_hosted_zone_public = var.dns_zone
+#   vpc_id                     = "data.terraform_remote_state.remote.vpc_id"
+#   alb_subnet_ids             = split(",", "data.terraform_remote_state.remote.alb_subnet_ids") # private subnet IDs
+#   ec2_subnet_ids             = tolist(data.terraform_remote_state.remote.ec2_subnet_ids) # private subnet IDs
+#   rds_subnet_ids             = "data.terraform_remote_state.remote.rds_subnet_ids" # private subnets IDs
+#   load_balancer_is_internal  = true
+#   route53_hosted_zone_public = var.dns_zone
 
-  ssh_key_pair = var.key
-  rds_master_password = var.rds_password
+#   ssh_key_pair = var.key
+#   rds_master_password = var.rds_password
 
-}
+# }
 
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "tfe_url" {
 output "tfe_admin_console_url" {
   value = module.tfe.tfe_admin_console_url
 }
+
+output "vpc" {
+    value = data.terraform_remote_state.remote.vpc_id
+}
+
+output "alb_subnets" {
+    value = data.terraform_remote_state.remote.alb_subnet_ids
+}
