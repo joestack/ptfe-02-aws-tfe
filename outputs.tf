1df1b709dca2621f17460677f1f10b77c17156a6
joestack <joern@joern-online.de>
2021-02-04 15:51:59 +0100

    14

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 
-output "tfe_url" {
-  value = module.tfe.tfe_url
-}
+# output "tfe_url" {
+#   value = module.tfe.tfe_url
+# }
 
-output "tfe_admin_console_url" {
-  value = module.tfe.tfe_admin_console_url
-}
+# output "tfe_admin_console_url" {
+#   value = module.tfe.tfe_admin_console_url
+# }
 
 output "vpc" {
-    value = data.terraform_remote_state.remote.vpc_id
+    value = data.terraform_remote_state.remote.outputs.vpc_id
 }
 
 output "alb_subnets" {
-    value = data.terraform_remote_state.remote.alb_subnet_ids
+    value = data.terraform_remote_state.remote.outputs.alb_subnet_ids
 }
