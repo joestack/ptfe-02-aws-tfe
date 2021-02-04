cd7258329ed075ac97d3b712a0aa5705cae24c75
joestack <joern@joern-online.de>
2021-02-04 15:56:46 +0100

    15

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 
-# output "tfe_url" {
-#   value = module.tfe.tfe_url
-# }
+output "tfe_url" {
+  value = module.tfe.tfe_url
+}
 
-# output "tfe_admin_console_url" {
-#   value = module.tfe.tfe_admin_console_url
-# }
+output "tfe_admin_console_url" {
+  value = module.tfe.tfe_admin_console_url
+}
 
 output "vpc" {
     value = data.terraform_remote_state.remote.outputs.vpc_id
